6146fd6b4b7ffefc7e732f944416f08693780770
Meseth <n.meseth@hs-osnabrueck.de>
2026-02-19 13:53:54 +0100

    Added chapters

--- project-1-survey/playground.R
+++ project-1-survey/playground.R
@@ -3,6 +3,10 @@ library(janitor)
 md <- read_csv("data/mds12_schoko_milch.csv", show_col_types = FALSE) |> 
   clean_names()
 
+library(tidyverse)
+survey <- read_csv("data/mds12_schoko_milch.csv", show_col_types = FALSE)
+
+
 
 md |> 
   colnames()
@@ -22,3 +26,91 @@ names(md) %>%
 md |> 
   names() |> 
   as_tibble_col("col")
+
+
+survey |>
+  ggplot() +
+  aes(x = u013pzahl_1weihen) +
+  geom_boxplot() +
+  coord_cartesian(xlim = c(0, 10))
+
+
+survey |>
+  ggplot() +
+  aes(x = u013pzahl_1weihen) +
+  geom_histogram()
+
+survey |>
+  filter(u013pzahl_1weihen < 10.0) |>
+  ggplot() +
+  aes(x = u013pzahl_1weihen) +
+  geom_histogram(binwidth = 0.5)
+
+
+
+survey |> 
+  select(q002geburt)
+
+format(Sys.Date(), "%Y")
+
+
+
+Sys.Date()
+
+current_year <- year(Sys.Date())
+
+survey |>
+  transmute(age = current_year - q002geburt)
+
+survey |>
+  transmute(age = 2025 - q002geburt) |>
+  distinct()
+
+
+library(janitor)
+
+install.packages("skimr")
+library(skimr)
+
+skimr::skim(my_data)
+
+
+survey |>
+  transmute(age = 2025 - q002geburt) |> 
+  tabyl(age)
+
+survey |>
+  transmute(age = 2025 - q002geburt) |> 
+  skim(age)
+
+
+survey |>
+  transmute(age = 2025 - q002geburt) |>
+  ggplot() +
+  aes(x = age) +
+  geom_bar()
+
+
+survey |>
+  transmute(age = 2025 - q002geburt) |>
+  ggplot() +
+  aes(x = age) +
+  geom_histogram()
+
+
+survey <- 
+  survey |>
+  mutate(Q002age = as.integer(2025 - q002geburt), .after = "q002geburt")
+
+
+survey |> 
+  select(Q002age) |> 
+  skim()
+
+
+survey |> 
+  select(tail(names(survey), 10))
+
+
+
+
